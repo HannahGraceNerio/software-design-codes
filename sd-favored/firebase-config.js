95971b6c39b95a6a9e1573106187f4e9d489a72c
Jalilah Disomnong <jalyydixx@gmail.com>
2026-02-16 22:22:25 +0800

    Update firebase-config.js

--- sd-favored/firebase-config.js
+++ sd-favored/firebase-config.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCq3RPAsO3n7UBDAyh0rStQakWlrFnuM5M",
   authDomain: "favored-guided-engraving-dae18.firebaseapp.com",
   projectId: "favored-guided-engraving-dae18",
-  storageBucket: "favored-guided-engraving-dae18.firebasestorage.app",
+  storageBucket: "favored-guided-engraving.firebasestorage.app",
   messagingSenderId: "526466641024",
   appId: "1:526466641024:web:faff4dc79039a37c0f4a65",
 };
@@ -22,4 +22,5 @@ const db = getFirestore(app);
 const storage = getStorage(app);
 
 // Export for use in app.js
+
 export { auth, db, storage };
